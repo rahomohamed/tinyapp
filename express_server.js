9e95e6f5f913a73ef53141d1e99188ed9b48aae0
rahomohamed <rahojmohamed@gmail.com>
2020-02-17 22:48:35 +0000

    updates changes

--- express_server.js
+++ express_server.js
@@ -26,6 +26,10 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+  res.render("urls_show", templateVars);
+});
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
