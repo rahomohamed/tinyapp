5de86f47dc88a3771f0f2122981f85acf17f6297
Raho <53961799+rahomohamed@users.noreply.github.com>
2020-02-18 18:14:34 -0500

    Update express_server.js

--- express_server.js
+++ express_server.js
@@ -1,28 +1,93 @@
-<header>
-  <nav class="navbar navbar-expand-md navbar-dark bg-success">
-    <a class="navbar-brand" href="/urls">TinyApp</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <a class="nav-item nav-link" href="/urls">My URLs</a>
-        <a class="nav-item nav-link" href="/urls/new">Create New URL</a>
-      </div>
-    </div>
-    <div>
-     <% if (username) { %> 
-      <form action="/logout" method="POST">
-         <a>Logged In As: <%= username %>! </a> 
-      <input type="submit" value ="Logout" class="btn btn-outline-dark">
-     </div>
-      <% } else { %>
-      <form method="POST" action="/login"><input type="text" placeholder="Username" name="username" required>  
-        <input type="submit" value="Login" class="btn btn-outline-dark">
-        <% } %>
-      </div>
-    </div>
-      </form>
-    </div>
-  </nav>
-</header>
+const express = require("express");
+const app = express();
+const cookieParser = require('cookie-parser')
+app.use(cookieParser())
+const PORT = 8080; // default port 8080
+
+app.set("view engine", "ejs");
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function generateRandomString () {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+};
+
+
+const urlDatabase = {
+  b2xVn2: "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase, username: req.cookies.username };
+  res.render("urls_index", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+let shortURL = generateRandomString()
+urlDatabase[shortURL] = req.body.longURL
+console.log(req.body.longURL);
+  res.redirect(`urls/${shortURL}`); 
+});
+
+app.post("/urls/:id", (req, res) => {
+  let shortURL = req.params.id;
+  let updatedlongURL = req.body.longURL;
+  urlDatabase[shortURL] = updatedlongURL;
+  res.redirect('/urls')
+})
+
+app.post("/login", (req, res) => {
+  let username = req.body.username
+  res.cookie('username', username)
+  res.redirect("/urls")
+})
+
+app.get("/logout", (req, res) => {
+  let username = req.body.username
+  res.clearCookie('username', username)
+  res.redirect("/urls")
+})
+
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls")
+})
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL]
+  res.redirect(longURL);
+});
+
+app.get("/urls/new", (req, res) => {
+  let templateVars = {username: req.cookies.username}
+  res.render("urls_new", tem);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL], 
+    username: req.cookies.username
+  };
+  res.render("urls_show", templateVars);
+});
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
